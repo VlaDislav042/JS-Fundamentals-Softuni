677be1a8d0656d646395b82bb23cf257623dca99
Vladislav Georgiev <vlafi90@gmail.com>
2023-02-27 19:57:11 +0200

    Add files via upload

---
+++ FUNDAMENTALS/Lessons/Seventh lection/personInfo.js
@@ -0,0 +1,12 @@
+function personInfo(firstName, lastName, age) {
+    let person = {
+        firstName1 : firstName,
+        lastName1 : lastName,
+        age1 : age,
+    }
+    return person;
+}
+console.log(personInfo("Peter", 
+"Pan",
+"20"
+));

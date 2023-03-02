58018fd1dbf1cec889ae1a9a1dd1526fcd5cd8de
Vladislav Georgiev <vlafi90@gmail.com>
2023-03-02 16:53:39 +0200

    Add files via upload

---
+++ FUNDAMENTALS/Exercises/Seventh lection/inventory.js
@@ -0,0 +1,31 @@
+function inventory(input) {
+    let res = [];
+    for (let line of input){
+        let[name, level, items] = line.split(" / ");
+        let heroes = {
+            name : name,
+            level : level,
+            items : items.split(", "),
+        }
+        res.push(heroes);
+    }
+    res.sort((heroesA, heroesB) => heroesA.level - heroesB.level);
+    for (const heroes of res) {
+        console.log(`Hero: ${heroes.name}`);
+        console.log(`level => ${heroes.level}`);
+        console.log(`items => ${heroes.items.join(", ")}`);
+    }
+}
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+    );
+console.log("--------");
+inventory([
+    'Batman / 2 / Banana, Gun',
+    'Superman / 18 / Sword',
+    'Poppy / 28 / Sentinel, Antara'
+    ]
+    );

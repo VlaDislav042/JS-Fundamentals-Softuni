3edfcf17722f1a2741fd52e30bc9c133b2d39c9d
Vladislav Georgiev <vlafi90@gmail.com>
2023-03-30 13:25:58 +0300

    Add files via upload

---
+++ FUNDAMENTALS/Lessons/Final exam/secretChat.js
@@ -0,0 +1,51 @@
+function secretChat(arr) {
+    let secretMessage = arr.shift();
+    for (const line of arr) {
+        let token = line.split(":|:");
+        let command = token[0];
+        let values = token.slice(1);
+        switch (command) {
+            case "ChangeAll":{
+                let substring = values[0];
+                let replacement = values[1];
+                while (secretMessage.includes(substring)) {
+                    secretMessage = secretMessage.replace(substring, replacement);
+                }
+                console.log(secretMessage);
+                break;
+            }
+            case "Reverse":{
+                let substring = values[0];
+                if (secretMessage.includes(substring)) {
+                    secretMessage = secretMessage.replace(substring, "");
+                    let reversed = reverseStirng(substring);
+                    secretMessage += reversed;
+                    console.log(secretMessage);
+                } else {
+                    console.log("error");
+                }
+                break;
+            }
+            case "InsertSpace":{
+                let index = values[0];
+                let leftSide = secretMessage.substring(0, index);
+                let rightSide = secretMessage.substring(index);
+                secretMessage = leftSide + " " + rightSide;
+                console.log(secretMessage);
+                break;
+            }
+            case "Reveal":{
+                console.log(`You have a new text message: ${secretMessage}`);
+                return;
+            }
+        }
+    }
+    function reverseStirng(text) {
+        return text.split("").reverse().join("");
+    }
+}
+secretChat(['heVVodar!gniV',
+    'ChangeAll:|:V:|:l',
+    'Reverse:|:!gnil',
+    'InsertSpace:|:5',
+    'Reveal']);
